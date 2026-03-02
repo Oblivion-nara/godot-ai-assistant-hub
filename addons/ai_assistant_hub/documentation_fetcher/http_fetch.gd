785067d7bd47dda55b7e6921f0352512923b9260
Reece <reecemeek@gmail.com>
2026-03-02 17:45:44 +0000

    initial work on adding godot docs to ai context

---
+++ addons/ai_assistant_hub/documentation_fetcher/http_fetch.gd
@@ -0,0 +1,40 @@
+class_name DocumentationController
+extends HTTPRequest
+
+@onready var version = Engine.get_version_info()
+@onready var file_location = $FileDialog
+@onready var url = "https://github.com/godotengine/godot-docs/archive/refs/heads/"+str(version.major)+"."+str(version.minor)+".zip"
+
+func _ready():
+	#print(version.major)
+	#print(version.minor)
+	print(url)
+	request_completed.connect(self._http_request_completed)
+	# Make the request
+	file_location.visible = true
+		
+func _http_request_completed(result, response_code, headers, body):
+	print("downloaded")
+	print(response_code)
+	var json = JSON.new()
+	json.parse(body.get_string_from_utf8())
+	var response = json.get_data()
+
+func _on_file_dialog_dir_selected(dir: String) -> void:
+	download_file = dir+"/download.zip"
+	print(download_file)
+	var error = request(url)
+	if error != OK:
+		push_error("An error occurred in the HTTP request.")
+
+
+func _on_file_dialog_canceled() -> void:
+	print("dialog_canceled")
+
+
+func _on_file_dialog_confirmed() -> void:
+	print("dialog_confirmed")
+
+
+func _on_file_dialog_custom_action(action: StringName) -> void:
+	print("custom_action")
